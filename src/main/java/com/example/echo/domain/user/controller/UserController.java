35f5097cbb604e859644ed4e682bfd38fe6540f2
백종우 <bsd03291@gmail.com>
2024-10-10 15:52:20 +0900

    ✨feat: User API Swagger 설정

--- src/main/java/com/example/echo/domain/user/controller/UserController.java
+++ src/main/java/com/example/echo/domain/user/controller/UserController.java
@@ -7,6 +7,8 @@ import com.example.echo.domain.user.dto.response.UserResDto;
 import com.example.echo.domain.user.service.command.UserCommandService;
 import com.example.echo.domain.user.service.query.UserQueryService;
 import com.example.echo.global.apiPayload.CustomResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -19,23 +21,27 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
+@Tag(name = "유저 API")
 public class UserController {
 
     private final UserCommandService userCommandService;
     private final UserQueryService userQueryService;
 
     @PostMapping("/login")
+    @Operation(summary = "로그인 API")
     public ResponseEntity<?> login(@RequestBody LoginRequestDto loginRequestDTO){
         return null;
     }
 
     @PostMapping("/logout")
+    @Operation(summary = "로그아웃 API")
     public ResponseEntity<?> logout(){
         return null;
     }
 
 
     @PostMapping("/signup")
+    @Operation(summary = "회원가입 API")
     public CustomResponse<UserResDto.UserResponseDto> createUser(@RequestBody UserReqDto.CreateUserRequestDto dto){
         UserResDto.UserResponseDto result = userCommandService.createUser(dto);
         return CustomResponse.onSuccess(HttpStatus.CREATED, result);
@@ -43,6 +49,7 @@ public class UserController {
 
 
     @GetMapping
+    @Operation(summary = "회원 조회 API")
     public CustomResponse<UserResDto.UserResponseDto> getUser(@AuthenticationPrincipal UserDetails userDetails){
         UserResDto.UserResponseDto result = userQueryService.getUser(userDetails.getUsername());
         return CustomResponse.onSuccess(HttpStatus.OK, result);
@@ -50,12 +57,14 @@ public class UserController {
 
 
     @PatchMapping
+    @Operation(summary = "회원 탈퇴 API")
     public CustomResponse<?> deleteUser(@AuthenticationPrincipal UserDetails userDetails){
         userCommandService.deleteUser(userDetails.getUsername());
         return CustomResponse.onSuccess(HttpStatus.OK, "성공적으로 회원이 탈퇴되었습니다.");
     }
 
     @PatchMapping("/nickname")
+    @Operation(summary = "회원 닉네임 수정 API")
     public CustomResponse<?> updateNickname(@AuthenticationPrincipal UserDetails userDetails,
                                             @RequestBody UserReqDto.UpdateNicknameRequestDto updateNicknameRequestDto){
         userCommandService.updateNickname(userDetails.getUsername(), updateNicknameRequestDto);
@@ -63,6 +72,7 @@ public class UserController {
     }
 
     @PutMapping("/password")
+    @Operation(summary = "회원 비밀번호 수정 API")
     public CustomResponse<?> updatePassword(@AuthenticationPrincipal UserDetails userDetails,
                                             @RequestBody UserReqDto.UpdatePasswordRequestDto updatePasswordRequestDto){
         userCommandService.updatePassword((userDetails.getUsername()), updatePasswordRequestDto);
