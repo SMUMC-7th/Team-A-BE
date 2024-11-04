ac4c520d3b8260edb9424fe41bd14b18dcc42242
MunChaerin <164905124+MunChaerin@users.noreply.github.com>
2024-11-04 15:48:38 +0900

    ♻️refactor: capsule soft delete 수정

--- src/main/java/com/example/echo/domain/capsule/entity/Tag.java
+++ src/main/java/com/example/echo/domain/capsule/entity/Tag.java
@@ -3,6 +3,8 @@ package com.example.echo.domain.capsule.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -15,7 +17,20 @@ public class Tag {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
     @Enumerated(EnumType.STRING)
     @Column(name="tag_name", columnDefinition = "VARCHAR(15)")
     private TagName tagName;
+
+    // 소프트 삭제 메서드
+    public void softDelete() {
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    // 복원 메서드
+    public void restore() {
+        this.deletedAt = null;
+    }
 }
