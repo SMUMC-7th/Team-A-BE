9982c773bbeaf5e2002a3571794fff2e5f50ea32
Jimyeong Kim <kjm021221@icloud.com>
2024-12-16 14:40:04 +0900

    ✨feat: 푸시알림 API Swagger Response 형식 추가

--- src/main/java/com/example/echo/domain/notification/controller/FcmController.java
+++ src/main/java/com/example/echo/domain/notification/controller/FcmController.java
@@ -10,6 +10,10 @@ import com.example.echo.domain.security.entity.AuthUser;
 import com.example.echo.global.apiPayload.CustomResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -27,6 +31,10 @@ public class FcmController {
     // FCM 토큰 저장 API
     @Operation(summary = "FCM 토큰 저장", description = "로그인된 사용자의 FCM 토큰을 저장합니다.")
     @PostMapping("/token")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "OK, 성공"),
+            @ApiResponse(responseCode = "NOTI_400", description = "잘못된 FCM 토큰 요청입니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+    })
     public CustomResponse<String> registerFcmToken(@CurrentUser AuthUser authUser,
                                                    @RequestBody NotificationReqDto.FcmTokenRequestDto fcmTokenRequestDto) {
         fcmTokenService.saveFcmToken(authUser.getEmail(), fcmTokenRequestDto.fcmToken());
@@ -36,31 +44,36 @@ public class FcmController {
     // FCM 푸시 알림 전송 API
     @Operation(summary = "FCM 푸시 알림 전송", description = "지정된 FCM 토큰으로 푸시 알림을 전송합니다.")
     @PostMapping("/send")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "OK, 성공"),
+            @ApiResponse(responseCode = "NOTI_400", description = "잘못된 FCM 토큰 요청입니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+            @ApiResponse(responseCode = "NOTI_401", description = "유효하지 않은 토큰입니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class)))
+    })
     public CustomResponse<String> sendNotification(@RequestBody NotificationReqDto.FcmSendDto fcmSendDto) {
         fcmService.sendFcmNotification(fcmSendDto);
         return CustomResponse.onSuccess("성공적으로 알림이 전송되었습니다.");
     }
 
-    // 최신 알림 조회 API
-    @Operation(summary = "최신 알림 조회", description = "커서 기반 페이징을 사용하여 최신 알림 목록을 조회합니다.")
-    @GetMapping("")
-    public CustomResponse<NotificationResDto.NotificationPreviewListDto> getNotifications(
-            @Parameter(description = "다음 페이지 조회에 사용할 커서 값")
-            @RequestParam(value = "cursor", required = false) Long cursor,
-
-            @Parameter(description = "한 페이지에 조회할 알림 개수")
-            @RequestParam(value = "offset", defaultValue = "10") int offset) {
-        // Service 계층에서 알림 조회
-        NotificationResDto.NotificationPreviewListDto response = fcmQueryService.getNotificationsByCursor(cursor, offset);
-
-        // 성공 응답 반환
-        return CustomResponse.onSuccess(response);
-    }
-
-    @Operation(summary = "알림 읽음 처리", description = "알림 ID에 해당하는 알림을 읽음 상태로 업데이트합니다.")
-    @PatchMapping("/{id}/read")
-    public CustomResponse<String> markNotificationAsRead(@Parameter(description = "읽음 처리할 알림 ID") @PathVariable Long id) {
-        fcmService.markAsRead(id);
-        return CustomResponse.onSuccess("알림이 읽음 상태로 업데이트되었습니다.");
-    }
+//    // 최신 알림 조회 API
+//    @Operation(summary = "최신 알림 조회", description = "커서 기반 페이징을 사용하여 최신 알림 목록을 조회합니다.")
+//    @GetMapping("")
+//    public CustomResponse<NotificationResDto.NotificationPreviewListDto> getNotifications(
+//            @Parameter(description = "다음 페이지 조회에 사용할 커서 값")
+//            @RequestParam(value = "cursor", required = false) Long cursor,
+//
+//            @Parameter(description = "한 페이지에 조회할 알림 개수")
+//            @RequestParam(value = "offset", defaultValue = "10") int offset) {
+//        // Service 계층에서 알림 조회
+//        NotificationResDto.NotificationPreviewListDto response = fcmQueryService.getNotificationsByCursor(cursor, offset);
+//
+//        // 성공 응답 반환
+//        return CustomResponse.onSuccess(response);
+//    }
+//
+//    @Operation(summary = "알림 읽음 처리", description = "알림 ID에 해당하는 알림을 읽음 상태로 업데이트합니다.")
+//    @PatchMapping("/{id}/read")
+//    public CustomResponse<String> markNotificationAsRead(@Parameter(description = "읽음 처리할 알림 ID") @PathVariable Long id) {
+//        fcmService.markAsRead(id);
+//        return CustomResponse.onSuccess("알림이 읽음 상태로 업데이트되었습니다.");
+//    }
 }
