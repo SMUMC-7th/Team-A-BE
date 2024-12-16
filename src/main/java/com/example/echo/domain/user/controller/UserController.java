3fa66a033992740499d2f7da77832dd39960803c
백종우 <bsd03291@gmail.com>
2024-12-16 14:18:06 +0900

    ✨ feat: User, Email API Swagger Response 형식 추가

--- src/main/java/com/example/echo/domain/user/controller/UserController.java
+++ src/main/java/com/example/echo/domain/user/controller/UserController.java
@@ -8,10 +8,17 @@ import com.example.echo.domain.security.entity.AuthUser;
 import com.example.echo.domain.user.dto.request.UserReqDto;
 import com.example.echo.domain.user.dto.response.UserResDto;
 import com.example.echo.domain.user.entity.AuthType;
+import com.example.echo.domain.user.exception.UserErrorCode;
+import com.example.echo.domain.user.exception.UserException;
 import com.example.echo.domain.user.service.command.UserCommandService;
 import com.example.echo.domain.user.service.query.UserQueryService;
 import com.example.echo.global.apiPayload.CustomResponse;
+import com.example.echo.global.apiPayload.exception.handler.GlobalExceptionHandler;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -32,12 +39,22 @@ public class UserController {
 
     @PostMapping("/login")
     @Operation(summary = "로그인 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "OK, 성공"),
+            @ApiResponse(responseCode = "LOGIN400", description = "올바르지 않은 요청입니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+            @ApiResponse(responseCode = "LOGIN401", description = "잘못된 정보입니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+    })
     public ResponseEntity<?> login(@RequestBody LoginRequestDto loginRequestDTO) {
         return null;
     }
 
     @PostMapping("/logout")
     @Operation(summary = "로그아웃 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "OK, 성공"),
+            @ApiResponse(responseCode = "SEC404_0", description = "리프레시 토큰이 존재하지 않습니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+            @ApiResponse(responseCode = "SEC401_0", description = "인증 자격 증명이 제공되지 않았거나 유효하지 않습니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+    })
     public ResponseEntity<?> logout() {
         return null;
     }
@@ -45,6 +62,11 @@ public class UserController {
 
     @PostMapping("/signup")
     @Operation(summary = "회원가입 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201",description = "CREATED, 성공"),
+            @ApiResponse(responseCode = "USER409_1", description = "이미 존재하는 이메일입니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+            @ApiResponse(responseCode = "COMMON500", description = "서버 내부 오류가 발생했습니다",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+    })
     public CustomResponse<UserResDto.UserResponseDto> createUser(@RequestBody UserReqDto.CreateUserRequestDto dto) {
         UserResDto.UserResponseDto result = userCommandService.createUser(dto);
         return CustomResponse.onSuccess(HttpStatus.CREATED, result);
@@ -52,6 +74,12 @@ public class UserController {
 
     @PostMapping("/kakao")
     @Operation(summary = "카카오 회원가입 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "OK, 성공"),
+            @ApiResponse(responseCode = "USER400_3", description = "잘못된 인증 방식입니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+            @ApiResponse(responseCode = "SEC401_0", description = "인증 자격 증명이 제공되지 않았거나 유효하지 않습니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+            @ApiResponse(responseCode = "USER403_1", description = "계정이 비활성화 상태입니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+    })
     public CustomResponse<JwtDto> kakao(@RequestBody UserReqDto.OAuthUserRequestDto dto) {
         JwtDto result = userCommandService.auth(dto, AuthType.KAKAO);
         return CustomResponse.onSuccess(HttpStatus.OK, result);
@@ -59,6 +87,12 @@ public class UserController {
 
     @PostMapping("/naver")
     @Operation(summary = "네이버 회원가입 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "OK, 성공"),
+            @ApiResponse(responseCode = "USER400_3", description = "잘못된 인증 방식입니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+            @ApiResponse(responseCode = "SEC401_0", description = "인증 자격 증명이 제공되지 않았거나 유효하지 않습니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+            @ApiResponse(responseCode = "USER403_1", description = "계정이 비활성화 상태입니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+    })
     public CustomResponse<JwtDto> naver(@RequestBody UserReqDto.OAuthUserRequestDto dto) {
         JwtDto result = userCommandService.auth(dto, AuthType.NAVER);
         return CustomResponse.onSuccess(HttpStatus.OK, result);
@@ -67,6 +101,11 @@ public class UserController {
 
     @GetMapping
     @Operation(summary = "회원 조회 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "OK, 성공"),
+            @ApiResponse(responseCode = "USER404_1", description = "사용자 데이터 값이 존재하지 않습니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+            @ApiResponse(responseCode = "SEC401_0", description = "인증 자격 증명이 제공되지 않았거나 유효하지 않습니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+    })
     public CustomResponse<UserResDto.UserResponseDto> getUser(@CurrentUser AuthUser authUser) {
         UserResDto.UserResponseDto result = userQueryService.getUser(authUser.getEmail());
         return CustomResponse.onSuccess(HttpStatus.OK, result);
@@ -75,6 +114,11 @@ public class UserController {
 
     @PatchMapping
     @Operation(summary = "회원 탈퇴 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "OK, 성공"),
+            @ApiResponse(responseCode = "USER404_1", description = "사용자 데이터 값이 존재하지 않습니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+            @ApiResponse(responseCode = "SEC401_0", description = "인증 자격 증명이 제공되지 않았거나 유효하지 않습니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+    })
     public CustomResponse<?> deleteUser(HttpServletRequest request,
                                         @CurrentUser AuthUser authUser) {
         userCommandService.deleteUser(request, authUser.getEmail());
@@ -83,6 +127,11 @@ public class UserController {
 
     @PatchMapping("/nickname")
     @Operation(summary = "회원 닉네임 수정 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "OK, 성공"),
+            @ApiResponse(responseCode = "USER404_1", description = "사용자 데이터 값이 존재하지 않습니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+            @ApiResponse(responseCode = "SEC401_0", description = "인증 자격 증명이 제공되지 않았거나 유효하지 않습니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+    })
     public CustomResponse<?> updateNickname(@CurrentUser AuthUser authUser,
                                             @RequestBody UserReqDto.UpdateNicknameRequestDto updateNicknameRequestDto) {
         userCommandService.updateNickname(authUser.getEmail(), updateNicknameRequestDto);
@@ -91,6 +140,12 @@ public class UserController {
 
     @PutMapping("/auth/password")
     @Operation(summary = "회원 비밀번호 수정 API", description = "마이페이지에서 비밀번호 수정")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "OK, 성공"),
+            @ApiResponse(responseCode = "USER400_1", description = "oldPassword가 기존 비밀번호가 다릅니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+            @ApiResponse(responseCode = "USER400_2", description = "newPassword가 기존이랑 같습니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+            @ApiResponse(responseCode = "USER404_1", description = "사용자 데이터 값이 존재하지 않습니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+    })
     public CustomResponse<?> updateAuthPassword(@CurrentUser AuthUser authUser,
                                             @RequestBody UserReqDto.UpdateAuthPasswordRequestDto dto) {
         userCommandService.updatePassword(authUser.getEmail(), dto);
@@ -99,6 +154,12 @@ public class UserController {
 
     @PutMapping("/password")
     @Operation(summary = "회원 비밀번호 수정 API", description = "비밀번호 찾기에서 이메일 인증 후 수정")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "OK, 성공"),
+            @ApiResponse(responseCode = "USER400_1", description = "oldPassword가 기존 비밀번호가 다릅니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+            @ApiResponse(responseCode = "USER400_2", description = "newPassword가 기존이랑 같습니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+            @ApiResponse(responseCode = "USER404_1", description = "사용자 데이터 값이 존재하지 않습니다.",content = @Content(schema = @Schema(implementation = CustomResponse.class))),
+    })
     public CustomResponse<?> updatePassword(@RequestBody UserReqDto.UpdatePasswordRequestDto dto) {
         userCommandService.setPassword(dto.email(), dto.password());
         return CustomResponse.onSuccess(HttpStatus.OK, "비밀번호 변경이 완료되었습니다.");
